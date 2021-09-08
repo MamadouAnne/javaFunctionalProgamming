4c126cf4ce439d831142eaf30de2b00d5269a932
Mamadou Anne <mamadiankha@gmail.com>
2021-09-08 09:27:14 -0700

    first commit

--- src/com/anne/Main.java
+++ src/com/anne/Main.java
@@ -1,8 +1,18 @@
 package com.anne;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter principal");
+        double principal = scanner.nextDouble();
+        System.out.println("Enter annual rate");
+        double annualRate = scanner.nextDouble();
+        System.out.println("Enter number of years");
+        short years = scanner.nextShort();
     }
-}
+
+
+    }
