c93d329789441ba4c255fbf71e15ab568eaf2b91
Mamadou Anne <mamadiankha@gmail.com>
2021-09-09 11:30:39 -0700

    adding a method

--- src/com/anne/Main.java
+++ src/com/anne/Main.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte percent = 100;
+        //final byte MONTH_IN_YEAR = 12;
+        //final byte percent = 100;
         double principal = 0;
-        double monthlyInterestRate =0;
-        int numberOfPayments = 0;
+        //double monthlyInterestRate =0;
+        //int numberOfPayments = 0;
+        double annualRate;
+        short years;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -23,27 +25,40 @@ public class Main {
         }
         while(true) {
             System.out.println("Enter annual rate");
-            double annualRate = scanner.nextDouble();
+            annualRate = scanner.nextDouble();
             if (annualRate >= 0 && annualRate <= 30){
-                monthlyInterestRate = annualRate/MONTH_IN_YEAR/percent;
+                //monthlyInterestRate = annualRate/MONTH_IN_YEAR/percent;
                 break;
             }
             System.out.println("Enter a number between 1 and 30.");
         }
         while (true) {
             System.out.println("Enter number of years");
-            int years = scanner.nextInt();
+            years = scanner.nextShort();
             if (years >= 1 && years <= 30){
-                numberOfPayments = years * MONTH_IN_YEAR;
+                //numberOfPayments = years * MONTH_IN_YEAR;
                 break;
             }
             System.out.println("Enter a number between 1 and 30.");
         }
 
-        double mortgage = principal*monthlyInterestRate*Math.pow((1 + monthlyInterestRate), numberOfPayments)/Math.pow((1 + monthlyInterestRate), numberOfPayments) -1;
+    double mortgage = calculateMortgageA(principal, annualRate, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println(mortgageFormatted);
 
+
+    }
+
+    public static double calculateMortgageA(double principal, double annualRate, short years){
+        final byte percent = 100;
+        final byte MONTH_IN_YEAR = 12;
+        int numberOfPayments = years * MONTH_IN_YEAR;
+        double monthlyInterestRate = annualRate/MONTH_IN_YEAR/percent;
+
+        double mortgage = principal*monthlyInterestRate*Math.pow((1 + monthlyInterestRate), numberOfPayments)/Math.pow((1 + monthlyInterestRate), numberOfPayments) -1;
+
+
+        return mortgage;
     }
 
 
