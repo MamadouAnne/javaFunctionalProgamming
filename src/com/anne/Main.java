fca6d31a9365ef26b234a5c458576448abe02c16
Mamadou Anne <mamadiankha@gmail.com>
2021-09-09 11:30:23 -0700

    validating the annuaRate and years

--- src/com/anne/Main.java
+++ src/com/anne/Main.java
@@ -9,6 +9,8 @@ public class Main {
         final byte MONTH_IN_YEAR = 12;
         final byte percent = 100;
         double principal = 0;
+        double monthlyInterestRate =0;
+        int numberOfPayments = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -19,13 +21,24 @@ public class Main {
                 break;
                 System.out.println("Enter a number between 1,000 and 1,000,000,000");
         }
-        System.out.println("Enter annual rate");
-        double annualRate = scanner.nextDouble();
-        System.out.println("Enter number of years");
-        int years = scanner.nextInt();
-
-        double monthlyInterestRate = annualRate/MONTH_IN_YEAR/percent;
-        int numberOfPayments = years * MONTH_IN_YEAR;
+        while(true) {
+            System.out.println("Enter annual rate");
+            double annualRate = scanner.nextDouble();
+            if (annualRate >= 0 && annualRate <= 30){
+                monthlyInterestRate = annualRate/MONTH_IN_YEAR/percent;
+                break;
+            }
+            System.out.println("Enter a number between 1 and 30.");
+        }
+        while (true) {
+            System.out.println("Enter number of years");
+            int years = scanner.nextInt();
+            if (years >= 1 && years <= 30){
+                numberOfPayments = years * MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a number between 1 and 30.");
+        }
 
         double mortgage = principal*monthlyInterestRate*Math.pow((1 + monthlyInterestRate), numberOfPayments)/Math.pow((1 + monthlyInterestRate), numberOfPayments) -1;
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
