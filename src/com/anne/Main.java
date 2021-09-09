45f0a5ccd2b7e7cabc87b62085ceda5023c1ee8b
Mamadou Anne <mamadiankha@gmail.com>
2021-09-09 10:37:28 -0700

    validating the principal

--- src/com/anne/Main.java
+++ src/com/anne/Main.java
@@ -8,11 +8,24 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTH_IN_YEAR = 12;
         final byte percent = 100;
+        double principal = 0;
+        double annualRate = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter principal");
-        double principal = scanner.nextDouble();
-        System.out.println("Enter annual rate");
-        double annualRate = scanner.nextDouble();
+
+        while (true) {
+            System.out.println("Principal ($1k - $1M):");
+            principal = scanner.nextDouble();
+            if (principal >= 1000 && principal <=1_000_000_000)
+                break;
+                System.out.println("Enter a number between 1,000 and 1,000,000,000.");
+        }
+        while (true) {
+            System.out.println("Enter annual rate");
+            annualRate = scanner.nextDouble();
+            if (annualRate >= 0 && annualRate <= 30)
+                break;
+            System.out.println("Enter a number between 0 and 30.");
+        }
         System.out.println("Enter number of years");
         int years = scanner.nextInt();
 
